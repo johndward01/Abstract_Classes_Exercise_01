069368710eea90da4da80a5243c6f5b8bbf772f2
johndward01 <johndward01@gmail.com>
2023-07-20 11:16:04 -0500

    Add project files.

---
+++ Abstract_Classes_Exercise_01/Program.cs
@@ -0,0 +1,13 @@
+﻿// TODO: Create an abstract class called Document 
+// TODO: Inside of the Document class, create a property called Title
+// TODO: Inside of the Document class, create a property called Author
+// TODO: Inside of the Document class, create an abstract method called Print()
+// TODO: Inside of the Document class, create an abstract method called Save()
+// TODO: Inside of the Document class, create an abstract method called Load()
+
+
+// TODO: Create 3 classes that inherit from the Document class: Word, PDF, and TextFile
+//       and implement the methods
+
+// TODO: Create 1 instance of each class, and call the 3 methods foreach
+
